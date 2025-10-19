79ee62b00d53a7daac5ba773be5b877b977222a3
Sungman-Hub <nomio4001@naver.com>
2025-10-19 21:50:42 +0900

    update
    Implemented main method to find common names from two lists.

--- week09_1019/BJ_1764.java
+++ week09_1019/BJ_1764.java
@@ -1,2 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Collections;
+import java.util.ArrayList;
+
 public class BJ_1764 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] Count = br.readLine().split(" ");
+        int hearCnt = Integer.parseInt(Count[0]);
+        int seeCnt = Integer.parseInt(Count[1]);
+        HashSet<String> set = new HashSet<>();
+        
+        for (int i = 0; i < hearCnt; i++) {
+            set.add(br.readLine());
+        }
+        
+        int cnt = 0;
+        ArrayList<String> res = new ArrayList<>();
+        
+        for (int i = 0; i < seeCnt; i++){
+            String noSee = br.readLine();
+            if (set.contains(noSee)) {
+                cnt++;
+                res.add(noSee);        
+            }
+        }
+        
+        Collections.sort(res);
+        System.out.println(cnt);
+        for(String result : res) {
+            System.out.println(result);
+        }
+        
+    }
 }
