b12e1975ba5bde67f6e9c06a5c71c227f03f095c
Sungman-Hub <nomio4001@naver.com>
2025-10-19 22:38:44 +0900

    update 신승민

--- week09_1019/BJ_2164.java
+++ week09_1019/BJ_2164.java
@@ -1,34 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
-public class BJ_10250 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int H = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
-            int N = Integer.parseInt(st.nextToken());
-
-            String h, w;
-
-            if (N % H != 0) {
-                h = String.valueOf(N % H);
-                w = String.valueOf(N / H + 1);
+        int num = Integer.parseInt(br.readLine());
+        
+        int power = 1;
+        while (power <= num) {
+            power *= 2;
+        }
+        power /= 2;
+        
+        if (num == 1) {
+            System.out.println(1);
+        } else if (num == 2) {
+            System.out.println(2);
+        } else {
+            if (num - power > 0) {
+                System.out.println((num - power) * 2);
             } else {
-                h = String.valueOf(H);
-                w = String.valueOf(N / H);
+                System.out.println(num);
             }
-
-            if (Integer.parseInt(w) < 10) {
-                w = "0" + w;
-            }
-
-            System.out.printf("%s%s\n", h, w);
         }
     }
 }
